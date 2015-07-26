d9107bf4240ad127a460737c8d07e289bf6035f9
Andrew McDonald <mcdonald.andrew.m@gmail.com>
2015-07-26 00:09:07 -0400

    easy challenge 4 solution

--- easy/challenge_4/solution.rb
+++ easy/challenge_4/solution.rb
@@ -2,10 +2,13 @@
 # random password generator
 # http://www.reddit.com/r/dailyprogrammer/comments/pm6oj/2122012_challenge_4_easy/
 
-require 'securerandom'
+chars = [(0..9), ('a'..'z'), ('A'..'Z')].map { |x| x.to_a }.concat(['!', '@', '#', '$', '%', '^', '&', '*', '?', '|']).flatten
 
 print 'How many passwords would you like to generate? '
-pw_quantity = gets.chomp
+pw_quantity = gets.to_i
 print 'How long should the passwords be? '
-pw_length = gets.chomp
+pw_length = gets.to_i
 
+pw_quantity.times do
+  puts (0...pw_length).map { chars.sample }.join
+end
